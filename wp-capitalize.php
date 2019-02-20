caf13d31c2d27e02c957e6e6adeb8461fb28817a
chigozieorunta <chigozieorunta@yahoo.com>
2019-02-20 18:33:35 +0100

    Update plugin file

---
+++ wp-capitalize.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Plugin Name: WP Capitalize
+ * Plugin URI: https://github.com/chigozieorunta/wp-capitalize
+ * Description: A simple WordPress plugin designed to help you capitalize post titles, author names and others...
+ * Version: 1.0
+ * Author: Chigozie Orunta
+ * Author URI: https://github.com/chigozieorunta
+*/
+
+//Define Plugin Path
+define("WPCAPITALIZE", ABSPATH.'wp-content/plugins/wp-capitalize');
+
+//Register Admin Menu
+add_action('admin_menu', 'wp_capitalize_menu');
+function wp_capitalize_menu() {
+    add_menu_page( 'WP Capitalize', 'WP Capitalize', 'manage_options', 'wp-capitalize', 'wp_capitalize_init' );
+}
+
+//Custom Functions
+require_once('wp-capitalize-functions.php');
+
+//Add Options Page
+function wp_capitalize_init() {
+    require_once('wp-capitalize-html.php');
+}
+
+?>
