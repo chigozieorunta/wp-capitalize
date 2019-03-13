00b38eadbe22e81fe8ed93e84b4aef87d038aec8
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-13 08:13:53 +0100

    Modified wp-capitalize.php parent class comments section

--- wp-capitalize.php
+++ wp-capitalize.php
@@ -1,12 +1,16 @@
 <?php
-
 /*
- * Plugin Name: WP Capitalize
- * Plugin URI: https://github.com/chigozieorunta/wp-capitalize
- * Description: A simple WordPress plugin designed to help you capitalize post titles, author names and others...
- * Version: 1.0
- * Author: Chigozie Orunta
- * Author URI: https://github.com/chigozieorunta
+ * Filename:      wp-capitalize.php
+ * Plugin Name:   wp-capitalize
+ * Plugin URI:    https://github.com/chigozieorunta/wp-capitalize
+ * Description:   A simple WordPress plugin designed to help you capitalize post titles, author names and others...
+ * Domain Path:   ./
+ * Description:   The parent class - wpCapitalize
+ * Author:        Chigozie Orunta
+ * Version:       1.0.0
+ * Licence:       MIT
+ * Author URI:    https://github.com/chigozieorunta
+ * Last Change:   2019/03/13
 */
 
 //Define Plugin Path
@@ -49,6 +53,102 @@ add_filter('the_title', 'wp_capitalize_post_title' );
 function wp_capitalize_post_title($title) {
 	$title = '<span style="text-transform: capitalize; ">'.$title.'</span>';
 	return $title;
+}
+
+wpGutenbergBlocks::getInstance();
+
+ /**
+ * Class wp_gutenberg_blocks
+ */
+class wpGutenbergBlocks {
+    /**
+	 * Private static variables
+	 *
+	 * @var string
+	 */
+    private static $pluginName;
+
+    /**
+	 * Constructor
+	 *
+	 * @since  1.0.0
+	 */
+    public function __construct() {
+        add_action('init', 'wp_gutenberg_blocks_init');
+        add_action('admin_menu', 'self::registerMenu');
+    }
+
+    /**
+	 * Init Method
+	 *
+     * @access private
+	 * @since  1.0.0
+	 */
+    private static function wp_gutenberg_blocks_init() {
+        self::registerScript();
+        self::registerBlockType();
+    }
+
+    /**
+	 * Register Script Method
+	 *
+     * @access private
+	 * @since  1.0.0
+	 */
+    private static function registerScript() {
+        wp_register_script(
+            'wp_gutenberg_blocks',
+            plugins_url('step-01/block.js', __FILE__),
+            array('wp-blocks', 'wp-element')
+        );
+    }
+
+    /**
+	 * Register Block Type Method
+	 *
+     * @access private
+	 * @since  1.0.0
+	 */
+    private static function registerBlockType() {
+        register_block_type(
+            'wp_gutenberg_blocks/hello-world-step-01', 
+            array(
+                'editor_script' => 'gutenberg-boilerplate-es5-step01',
+            ) 
+        );
+    }
+
+    /**
+	 * Register Menu Method
+	 *
+     * @access private
+	 * @since  1.0.0
+	 */
+    private static function registerMenu() {
+        add_menu_page('wp_gutenberg_blocks', 'wp_gutenberg_blocks', 'manage_options', 'wp_gutenberg_blocks', 'self::registerHTML');
+    }
+
+    /**
+	 * Register HTML Method
+	 *
+     * @access private
+	 * @since  1.0.0
+	 */
+    private static function registerHTML() {
+        require_once('wp-gutenberg-blocks-html.php');
+    }
+
+    /**
+	 * Points the class, singleton.
+	 *
+	 * @access public
+	 * @since  1.0.0
+	 */
+    public static function getInstance() {
+        static $instance;
+        if($instance === null) $instance = new self();
+        return $instance;
+    }
 }
 
 ?>
