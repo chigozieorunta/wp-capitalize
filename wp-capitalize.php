be86dff6359051ec9464afc9aa07a7662acfe0bd
chigozieorunta <chigozieorunta@yahoo.com>
2020-02-24 09:37:04 +0100

    Modified wpCapitalize function

--- wp-capitalize.php
+++ wp-capitalize.php
@@ -5,7 +5,7 @@
  * Plugin URI:    https://github.com/chigozieorunta/wp-capitalize
  * Description:   A simple WordPress plugin designed to help you capitalize post titles, author names and others...
  * Domain Path:   ./
- * Description:   The parent class - wpCapitalize
+ * Description:   The function - wpCapitalize
  * Author:        Chigozie Orunta
  * Version:       1.0.0
  * Licence:       MIT
@@ -13,68 +13,9 @@
  * Last Change:   2019/03/13
 */
 
-//Define Plugin Path
-define("WPCAPITALIZE", plugin_dir_url( __FILE__ ));
-
-wpCapitalize::getInstance();
-
-/**
- * Class wpCapitalize
- */
-class wpCapitalize {
-    /**
-	 * Private static variables
-	 *
-	 * @var string
-	 */
-    private static $pluginName;
-
-    /**
-	 * Constructor
-	 *
-	 * @since  1.0.0
-	 */
-    public function __construct() {
-        add_action('admin_menu', array(get_called_class(), 'registerMenu'));
-    }
-
-    /**
-	 * Register Menu Method
-	 *
-     * @access private  
-	 * @since  1.0.0
-	 */
-    public static function registerMenu() {
-        add_menu_page(
-            'wp-capitalize', 
-            'wp-capitalize', 
-            'manage_options', 
-            'wp-capitalize', 
-            array(get_called_class(), 'registerHTML')
-        );
-    }
-
-    /**
-	 * Register HTML Method
-	 *
-     * @access private
-	 * @since  1.0.0
-	 */
-    public static function registerHTML() {
-        require_once('wp-capitalize-html.php');
-    }
-
-    /**
-	 * Points the class, singleton.
-	 *
-	 * @access public
-	 * @since  1.0.0
-	 */
-    public static function getInstance() {
-        static $instance;
-        if($instance === null) $instance = new self();
-        return $instance;
-    }
+add_filter('the_title', 'wpCapitalize', 10, 2);
+function wpCapitalize($title) {
+    return ucwords($title);
 }
 
 ?>
