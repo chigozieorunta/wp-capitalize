dea2e0a10da0deb22255b7da6c843616b33e97a6
chigozieorunta <chigozieorunta@yahoo.com>
2020-02-24 09:42:50 +0100

    removed prev js defn

--- js/wp-capitalize.js
+++
@@ -1,8 +0,0 @@
-(function($) {	
-	$(document).ready(function() {
-		$(".switch").click(function() {
-			$(this).toggleClass("switch-on");
-			$(this).children(".switch-btn").toggleClass("switch-btn-on");
-		});
-	})
-})( jQuery );
